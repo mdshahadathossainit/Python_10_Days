a2a571f9f1d8d7ae7c5b10f0ad810824370bf053
Md Shahadat Hossain <m.shahadat.hossain.it@gmail.com>
2025-09-13 15:04:58 +0600

    "Day 1: Basics and exercises"

---
+++ day1_basics.py
@@ -0,0 +1,7 @@
+#day1_basic.py
+print("===Day1: Basics===")
+#task A - name and age and roll
+name=input("write your name:")
+age=int(input("write your age:"))
+roll=int(input("write your roll"))
+print(f"Hello {name},{age+5},{roll}")
