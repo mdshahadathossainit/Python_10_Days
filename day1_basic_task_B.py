907134431f7ce7587b20275819bad655d40ea00b
Md Shahadat Hossain <m.shahadat.hossain.it@gmail.com>
2025-09-13 15:04:58 +0600

    "Day 1: Basics and exercises"

---
+++ day1_basic_task_B.py
@@ -0,0 +1,9 @@
+#task B - bigger of two numbers
+a = float(input("first number:"))
+b = float(input("second Number:"))
+if a>b:
+     print ("Big number:",a)
+elif a<b:
+     print("big number:",b)
+else:
+     print("equal")
